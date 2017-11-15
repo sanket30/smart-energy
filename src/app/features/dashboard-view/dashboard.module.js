25d668fbd953dbabdec27dbf1ba158a2d1eb3346
Sanket Tajane <c.sanket.tajane@uptake.com>
2017-11-14 20:14:50 -0600

    feat(plot-lines): Remove extra module

--- src/app/features/dashboard-view/dashboard.module.js
+++ src/app/features/dashboard-view/dashboard.module.js
@@ -2,7 +2,6 @@
     'use strict';
 
     angular.module('smartEnergy.dashboard', [
-        'smartEnergy.timeseries',
         'smartEnergy.app.dateRangeSelector',
         'smartEnergy.app.totalImpact',
         'smartEnergy.timeseries'
